9cf1c17eeb466de0e6e962186def794df61f35fd
binod <binodcoder@gmail.com>
2022-03-01 17:34:40 +0545

    no

---
+++ app/src/main/java/com/binodcoder/fragmentdemo1/ActivityHomePage.java
@@ -0,0 +1,14 @@
+package com.binodcoder.fragmentdemo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
